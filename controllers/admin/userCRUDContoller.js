e36e36bed3f436c7faff9b53bb66a2bf90e3ba50
obeespace <94024823+obeespace@users.noreply.github.com>
2025-09-25 12:00:44 +0100

    crud controller updated

--- controllers/admin/userCRUDContoller.js
+++ controllers/admin/userCRUDContoller.js
@@ -1,4 +1,6 @@
 import User from "../../models/userModel.js";
+import jwt from "jsonwebtoken";
+
 const generateToken = (id, role) => {
   return jwt.sign({ id, role }, process.env.JWT_SECRET, { expiresIn: "1d" });
 };
